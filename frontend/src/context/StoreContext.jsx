af8f1b7245af6fc6e1d3b6b7616e8fce36af5212
BaoWhiteHat <23560003@gm.uit.edu.vn>
2024-07-04 13:21:14 +0700

    ok

---
+++ frontend/src/context/StoreContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useEffect, useState } from 'react'
+import { food_list} from '../assets/assets'
+ 
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+
+    const [cartItems, setCartItems] = useState({});
+
+    // Add to Cart
+    const addToCart = (itemId) => {
+        if(!cartItems[itemId]){
+            setCartItems((prev)=>({...prev, [itemId]:1}))
+        }
+        else {
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+    }
+
+    // Remove from Cart
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
+
+    const contextValue = {
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart, 
+        removeFromCart
+    }
+
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+export default StoreContextProvider
