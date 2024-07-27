a85118a747c0439afb9fdd9012ce7a73fe3d6875
BaoWhiteHat <23560003@gm.uit.edu.vn>
2024-07-27 23:44:35 +0700

    Create CheckOut page (Stripe) and fix bugs (2 weeks)

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,9 @@
+import express from "express"
+import authMiddelware from "../middleware/auth.js"
+import { placeOrder } from "../controllers/orderController.js"
+
+const orderRouter = express.Router();
+
+orderRouter.post("/place", authMiddelware, placeOrder);
+
+export default orderRouter;
