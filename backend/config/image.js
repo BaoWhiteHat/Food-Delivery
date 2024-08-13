f899b9df2d0764a1179b0f14182ad743552a264b
Le Quoc Bao <lequocbao2222005@gmail.com>
2024-08-13 19:44:12 +0700

    Change some Cloundinary methods

--- backend/config/image.js
+++ backend/config/image.js
@@ -2,9 +2,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-    cloud_name: 'dgncir2mb',
-    api_key: '957991786227583', // API Key mới
-    api_secret: 'CyA1LFFtDAYGmliWhE7JlPozA2E' // API Secret mới
+    cloud_name: 'dvjjg3x3p',
+    api_key: '714521117572225', // API Key mới
+    api_secret: 'zNdTI3WR4eEuCWrzzrbFjQgG9cM' // API Secret mới
 });
 
 console.log('Configured Cloudinary with:', cloudinary.config());
